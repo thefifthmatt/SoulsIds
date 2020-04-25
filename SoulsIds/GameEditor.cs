55651bcc3829269efa2ab9359d631bce9246dd0b
Matthew Gruen <matt@thefifthmatt.com>
2020-04-24 20:53:22 -0700

    Update with event tools

--- SoulsIds/GameEditor.cs
+++ SoulsIds/GameEditor.cs
@@ -21,11 +21,19 @@ namespace SoulsIds
         {
             this.Spec = spec;
         }
-        public Dictionary<string, PARAM> LoadParams(Dictionary<string, PARAM.Layout> layouts = null)
+
+        // Load params from a combination of game dir and param file in spec.
+        public Dictionary<string, PARAM> LoadParams(Dictionary<string, PARAM.Layout> layouts = null, bool allowError = false)
         {
             if (Spec.ParamFile == null) throw new Exception("Param path unknown");
+            return LoadParams($@"{Spec.GameDir}\{Spec.ParamFile}", layouts, allowError);
+        }
+
+        // Load params from given path, relative to current dir
+        public Dictionary<string, PARAM> LoadParams(string path, Dictionary<string, PARAM.Layout> layouts = null, bool allowError = false)
+        {
             layouts = layouts ?? LoadLayouts();
-            return LoadBnd($@"{Spec.GameDir}\{Spec.ParamFile}", (data, path) =>
+            return LoadBnd(path, (data, paramPath) =>
             {
                 PARAM param;
                 try
@@ -34,7 +42,7 @@ namespace SoulsIds
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e);
+                    if (!allowError) throw new Exception($"Failed to load param {paramPath}: " + e);
                     // For DS3 this also includes draw params, so just silently fail
                     // TODO: Find a better way to load all params reliably
                     return null;
@@ -59,6 +67,8 @@ namespace SoulsIds
                 return null;
             });
         }
+
+        // Loads names from the given txt file name in NameDir, relative to the current dir
         public Dictionary<T, string> LoadNames<T>(string name, Func<string, T> key, bool allowMissing = false)
         {
             if (Spec.NameDir == null) throw new Exception("Name file dir not provided");
@@ -78,6 +88,8 @@ namespace SoulsIds
             }
             return ret;
         }
+
+        // Loads layouts from layout dir, relative to the current dir
         public Dictionary<string, PARAM.Layout> LoadLayouts()
         {
             if (Spec.LayoutDir == null) throw new Exception("Layout dir not provided");
@@ -90,6 +102,8 @@ namespace SoulsIds
             }
             return layouts;
         }
+
+        // Loads multiple files from relDir, relative to the game directory
         public Dictionary<string, T> Load<T>(string relDir, Func<string, T> reader, string ext = "*.dcx")
         {
             if (Spec.GameDir == null) throw new Exception("Base game dir not provided");
@@ -108,7 +122,9 @@ namespace SoulsIds
             }
             return ret;
         }
-        public Dictionary<string, T> LoadBnd<T>(string path, Func<byte[], string, T> parser, string fileExt=null)
+
+        // Loads a bnd file from file path, relative to the current dir
+        public Dictionary<string, T> LoadBnd<T>(string path, Func<byte[], string, T> parser, string fileExt = null)
         {
             string name = BaseName(path);
             Dictionary<string, T> bnds = new Dictionary<string, T>();
@@ -132,11 +148,14 @@ namespace SoulsIds
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"Failed to load {path}: {bndName}: {ex}");
+                    // TODO: Check this is okay, rather than printing
+                    throw new Exception($"Failed to load {path}: {bndName}: {ex}");
                 }
             }
             return bnds;
         }
+
+        // Loads multiple bnd files from file path, relative to game dir
         public Dictionary<string, Dictionary<string, T>> LoadBnds<T>(string relDir, Func<byte[], string, T> parser, string ext = "*bnd.dcx", string fileExt = null)
         {
             if (Spec.GameDir == null) throw new Exception("Base game dir not provided");
@@ -152,6 +171,8 @@ namespace SoulsIds
             }
             return ret;
         }
+
+        // Reads bnd
         private IBinder ReadBnd(string path)
         {
             try
@@ -175,7 +196,17 @@ namespace SoulsIds
                 throw new Exception($"Failed to load {path}: {ex}");
             }
         }
-        public void OverrideBnd<T>(string path, string toDir, Dictionary<string, T> diffData, Func<T, byte[]> writer, string fileExt=null)
+
+        // Overrides bnd, relative to current dir, and outputs to directory, relative to game dir if not absolute path
+        public void OverrideBnd<T>(string path, string toDir, Dictionary<string, T> diffData, Func<T, byte[]> writer, string fileExt = null)
+        {
+            string fname = Path.GetFileName(path);
+            string outPath = AbsolutePath(Spec.GameDir, $@"{toDir}\{fname}");
+            OverrideBndRel(path, outPath, diffData, writer, fileExt);
+        }
+
+        // Overrides bnd, relative to current dir, and outputs to other file
+        public void OverrideBndRel<T>(string path, string outPath, Dictionary<string, T> diffData, Func<T, byte[]> writer, string fileExt=null)
         {
             if (Spec.Dcx == DCX.Type.Unknown) throw new Exception("DCX encoding not provided");
             string fname = Path.GetFileName(path);
@@ -194,12 +225,16 @@ namespace SoulsIds
                     Console.WriteLine($"Failed to load {path}: {bndName}: {ex}");
                 }
             }
-            string outPath = AbsolutePath(Spec.GameDir, $@"{toDir}\{fname}");
             if (bnd is BND4 bnd4)
             {
                 if (Spec.Game == FromGame.DS3 && outPath.EndsWith("Data0.bdt"))
                 {
                     SFUtil.EncryptDS3Regulation(outPath, bnd4);
+                    if (new FileInfo(outPath).Length > 0x10000C)
+                    {
+                        File.Delete(outPath);
+                        throw new Exception($"You must set loadLooseParams=1 in modengine.ini. Otherwise Data0.bdt is too large and will permanently corrupt your save file.");
+                    }
                 }
                 else
                 {
@@ -208,6 +243,8 @@ namespace SoulsIds
             }
             if (bnd is BND3 bnd3) bnd3.Write(outPath, Spec.Dcx);
         }
+
+        // Overrides bnds, relative to game dir, and outputs to directory, relative to game dir if relative
         public void OverrideBnds<T>(string fromDir, string toDir, Dictionary<string, Dictionary<string, T>> diffBnds, Func<T, byte[]> writer, string ext = "*bnd.dcx", string fileExt = null)
         {
             if (Spec.GameDir == null) throw new Exception("Base game dir not provided");
@@ -218,17 +255,24 @@ namespace SoulsIds
                 OverrideBnd(path, toDir, diffBnds[name], writer, fileExt);
             }
         }
+
+        // Return a path name without, for easy access within code.
+        // This should be avoided when there are multiple files with the same base name.
         public static string BaseName(string path)
         {
             path = Path.GetFileName(path);
             if (path.IndexOf('.') == -1) return path;
             return path.Substring(0, path.IndexOf('.'));
         }
+
+        // Given an default absolute path and relative or absolute path, resolve the latter path if necessary.
         public static string AbsolutePath(string basePath, string maybeRelPath)
         {
             if (basePath == null) return Path.GetFullPath(maybeRelPath);
             return Path.GetFullPath(Path.Combine(basePath, maybeRelPath));
         }
+
+        // Param util: copy all values from one row to another
         public static void CopyRow(PARAM.Row from, PARAM.Row to)
         {
             for (int i = 0; i < from.Cells.Count; i++)
